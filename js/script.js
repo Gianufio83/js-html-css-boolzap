10bf8bbec9a095db4aec42f31b4e07fef2b19fee
Gianufio83 <fiore.gianuario@gmail.com>
2020-01-24 16:00:16 +0100

    prova risposta automatica ok

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
   $('.send-text').click(function () {
     sendMessage();
   });
+  setTimeout(getAnswer, 1000);
 });
 
 function sendMessage() {
@@ -32,9 +33,18 @@ function addZero(number) {
   }
   return number;
 }
+// Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
+// Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
 
-
-
+function getAnswer() {
+  var sendText = $('.sent');
+  if (sendText == true) {
+    var newAnswerText = $('.template .message').clone();
+    newAnswerText.find('.message-text').text('OK');
+    newAnswerText.addClass('received');
+    $('.conversation').append(newMessage);
+  }
+}
 
 
 
